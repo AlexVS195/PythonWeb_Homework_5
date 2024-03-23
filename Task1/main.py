95a52bbf4f686a16bb71a5f7572783ae40cf5bd3
AlexVS195 <142892792+AlexVS195@users.noreply.github.com>
2024-03-23 21:31:42 +0200

    Add files via upload

--- Task1/main.py
+++ Task1/main.py
@@ -1,1 +1,17 @@
+from Bot import Bot
 
+if __name__ == "__main__":
+    print('Hello. I am your contact-assistant. What should I do with your contacts?')
+    bot = Bot()
+    bot.book.load("auto_save")
+    commands = ['Add', 'Search', 'Edit', 'Load', 'Remove', 'Save', 'Congratulate', 'View', 'Exit']
+    while True:
+        action = input('Type help for list of commands or enter your command\n').strip().lower()
+        if action == 'help':
+            print(" ".join(f"{command:^20}" for command in commands))
+            action = input().strip().lower()
+        bot.handle(action)
+        if action in ['add', 'remove', 'edit']:
+            bot.book.save("auto_save")
+        if action == 'exit':
+            break
